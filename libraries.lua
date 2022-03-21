b2b6f5ca62ac34a895264f9ee1b54afc6a70851b
Lunaxyz1 <93170295+Lunaxyz1@users.noreply.github.com>
2022-03-21 17:12:51 -0400

    Add files via upload

---
+++ libraries.lua
@@ -0,0 +1,47 @@
+local drawing = {Objects = {}}
+getgenv().Objects = drawing.Objects
+local maths = {}
+local Lib = {}
+
+function drawing:NewDrawing(player, type)
+    local _Drawing = Drawing.new(type)
+    drawing.Objects[player] = _Drawing
+    return _Drawing
+end
+
+function drawing:GetDrawing(player)
+    return drawing.Objects[player]
+end 
+maths.GetCorners = function(Object) 
+    local Position = Object.Position
+    local Size = Object.Size
+    local TopX, TopY = Position.X - (Size.X / 2), Position.Y - (Size.Y / 2)
+    local BottomY = TopY - Size.Y
+
+    return {
+        TopLeft = Vector3.new(TopX, TopY, Position.Z),
+		TopRight = Vector3.new(TopX + Size.X, TopY, Position.Z),
+		BottomLeft = Vector3.new(TopX, BottomY, Position.Z),
+		BottomRight = Vector3.new(TopX + Size.X, BottomY, Position.Z)
+    } 
+end
+
+maths.GetClosestWithin = function(CompareFrom, CompareTo, TargetDistance)
+    local Distance = TargetDistance or math.huge
+    local Closest
+    
+    local Dist = (CompareFrom.Position - CompareTo.Position).Magnitude
+    if Dist < Distance then
+        Distance = Dist
+        Closest = CompareTo
+    end
+    return Closest
+end
+
+
+
+
+return {
+    Drawings = drawing,
+    maths = maths,
+}
